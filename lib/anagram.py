b1ed1f970eab8a8819448ce7e7fc17856d58e34e
Marvin-Kabugi <kabugijeff@gmail.com>
2023-05-21 17:48:55 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word) -> None:
+        self.word = set(word)
+    
+    def match(self, word_list):
+        anagram = []
+        for word in word_list:
+            if set(word) == self.word:
+                anagram.append(word)
+        return anagram
+
+
